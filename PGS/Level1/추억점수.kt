0d1d48c092425adcc398de69f935ddcbc9695d9b
arab1209 <8345784@naver.com>
2024-06-23 11:31:07 +0900

    추억점수

--- PGS/Level1/추억점수.kt
+++ PGS/Level1/추억점수.kt
@@ -1,2 +1,16 @@
 class 추억점수 {
+    fun solution(name: Array<String>, yearning: IntArray, photo: Array<Array<String>>): IntArray {
+        var answer: IntArray = intArrayOf()
+
+        for (i in photo) {
+            var sum: Int = 0
+            for (j in i) {
+                if (name.contains(j)) {
+                    sum += yearning[name.indexOf(j)]
+                }
+            }
+            answer += sum
+        }
+        return answer
+    }
 }
