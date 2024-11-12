4f82b1e10020e561c80b0471a5cee2acb441eec7
song-gichan <8345784@naver.com>
2024-11-12 11:34:54 +0900

    [refactor] K번째 수

--- PGS/Level1/K번째 수.kt
+++ PGS/Level1/K번째 수.kt
@@ -2,14 +2,6 @@ class `K번째 수` {
     fun solution(array: IntArray, commands: Array<IntArray>): IntArray {
         var answer = intArrayOf()
 
-        commands.forEach {
-            val temp = it.joinToString().split(", ").map { it.toInt() }
-            val start = temp[0] - 1
-            val end = temp[1]
-            val resultList = array.slice(start until end).sorted()
-
-            answer += resultList[temp.last()-1]
-        }
-        return answer
+        return commands.map { array.slice(it[0]-1..it[1]-1).sorted()[it.last()-1] }.toIntArray()
     }
 }
