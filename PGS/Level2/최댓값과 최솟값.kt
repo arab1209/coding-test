f3bbb5a46b403bd3e00df724262d686e16486672
song-gichan <8345784@naver.com>
2024-11-15 10:29:37 +0900

    [refactor] 최댓값과 최솟값

--- PGS/Level2/최댓값과 최솟값.kt
+++ PGS/Level2/최댓값과 최솟값.kt
@@ -2,7 +2,6 @@ class `최댓값과 최솟값` {
     fun solution(s: String): String {
         var answer = ""
 
-        return s.split(" ").map { it.toInt() }.minOrNull().toString() + " " + s.split(" ").map { it.toInt() }
-            .maxOrNull().toString()
+        return s.split(" ").map { it.toInt() }.let { "${it.minOrNull()} ${it.maxOrNull()}" }
     }
 }
