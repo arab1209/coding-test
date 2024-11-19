38ebf824c52e9c6a21af9d98bebfb678f8acb16c
song-gichan <8345784@naver.com>
2024-11-19 15:54:14 +0900

    [refactor] 대충 만든 자판

--- PGS/Level1/대충 만든 자판.kt
+++ PGS/Level1/대충 만든 자판.kt
@@ -2,30 +2,14 @@ class `대충 만든 자판` {
     fun solution(keymap: Array<String>, targets: Array<String>): IntArray {
         var answer: IntArray = intArrayOf()
 
-        val temp = mutableListOf<Int>()
-        var check: Boolean = false
-        var min = 0
-
-        targets.forEach { i ->
-            i.forEach { j ->
-                keymap.forEach { k ->
-                    if (k.contains(j)) temp.add(k.indexOf(j) + 1)
-                }
-
-                var q = temp.minOrNull()
-
-                if (q != null) {
-                    min += q
-                } else {
-                    min = -1
-                    check = true
-                }
-                temp.clear()
+        return targets.map { s ->
+            s.map { c ->
+                keymap.map { k ->
+                    k.indexOf(c) + 1
+                }.filterNot { it < 1 }.minOrNull()
+            }.let {
+                if (it.contains(null)) -1 else it.filterNotNull().sum()
             }
-            if (check) answer += -1 else answer += min
-            check = false
-            min = 0
-        }
-        return answer
+        }.toIntArray()
     }
 }
