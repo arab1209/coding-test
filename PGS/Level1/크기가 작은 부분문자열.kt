e6683e8a7c0d8a25c64ce58cff678777934e2cdf
song-gichan <8345784@naver.com>
2024-11-28 09:47:58 +0900

    [refactor] 크기가 작은 부분문자열

--- PGS/Level1/크기가 작은 부분문자열.kt
+++ PGS/Level1/크기가 작은 부분문자열.kt
@@ -2,12 +2,8 @@ class `크기가 작은 부분문자열` {
     fun solution(t: String, p: String): Int {
         var answer: Int = 0
 
-        for (i in 0..t.length - p.length) {
-            if (t.substring(i, i + p.length) <= p) {
-                answer++
-            }
-        }
-
-        return answer
+        return (0..t.length - p.length).filter {
+            t.substring(it, it + p.length) <= p
+        }.count()
     }
 }
